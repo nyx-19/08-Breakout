a6fce8b7f686a83e492b603de2ce096be0a3317f
nyx-19 <54558770+nyx-19@users.noreply.github.com>
2019-10-30 23:49:48 -0400

    finished

---
+++ Scripts/World.gd
@@ -0,0 +1,18 @@
+extends Node
+
+export var score = 0
+export var lives = 3
+
+func increase_score(s):
+ score += int(s)
+ find_node("Score").update_score()
+ if score == 900:
+   get_tree().change_scene("res://Scenes/Level2.tscn") 
+
+func decrease_lives():
+ lives -= 1
+ find_node("Lives").update_lives()
+ if lives <= 0:
+  get_tree().change_scene("res://Scenes/End.tscn")
+ 
+ 
